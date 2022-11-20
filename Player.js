8d2a01924ab39eda51e23f6cc3356df94bdf694c
TilenS2002 <tilen.smrdel2@gmail.com>
2022-11-20 15:57:49 +0100

    movement dodelan

--- Player.js
+++ Player.js
@@ -1,4 +1,33 @@
+import {} from './Matrix.mjs'
+
 // tle bova programirala playerja
 class movement {
-    constructor()
+    constructor(){
+        this.tipka = {};
+    };
+    initHandlers() {
+        this.pointermoveHandler = this.pointermoveHandler.bind(this);
+        this.keydownHandler = this.keydownHandler.bind(this);
+        this.keyupHandler = this.keyupHandler.bind(this);
+
+        const element = this.domElement;
+        const doc = element.ownerDocument;
+
+        doc.addEventListener('keydown', this.keydownHandler);
+        doc.addEventListener('keyup', this.keyupHandler);
+
+        element.addEventListener('click', e => element.requestPointerLock());
+        doc.addEventListener('pointerlockchange', e => {
+            if (doc.pointerLockElement === element) {
+                doc.addEventListener('pointermove', this.pointermoveHandler);
+            } else {
+                doc.removeEventListener('pointermove', this.pointermoveHandler);
+            }
+        });
+    }
+}
+class camera {
+    constructor() {
+
+    };
 }
