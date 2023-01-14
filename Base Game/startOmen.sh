8fad0d782163c7720896ef5bc4c697b626ba50eb
TilenS2002 <tilen.smrdel2@gmail.com>
2023-01-14 22:22:12 +0100

    delam script za linux, ma ne dela se cist

--- Base Game/startOmen.sh
+++ Base Game/startOmen.sh
@@ -3,18 +3,18 @@ then
     node ./bin/server.js
 else
     declare -A osInfo;
-    osInfo[/etc/redhat-release]=yum
-    osInfo[/etc/arch-release]=pacman
-    osInfo[/etc/gentoo-release]=emerge
-    osInfo[/etc/SuSE-release]=zypp
-    osInfo[/etc/debian_version]=apt-get
-    osInfo[/etc/alpine-release]=apk
+    osInfo[/etc/redhat-release]=yum install -y gcc-c++ make; curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash -&&\ ; sudo yum install nodejs
+    osInfo[/etc/arch-release]=pacman -S nodejs npm
+    osInfo[/etc/gentoo-release]=emerge nodejs
+    osInfo[/etc/SuSE-release]=zypp install nodejs14
+    osInfo[/etc/debian_version]=apt install curl; curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\ ; apt install nodejs
+    osInfo[/etc/alpine-release]=apk add nodejs npm
 
     for f in ${!osInfo[@]}
     do
         if [[ -f $f ]];then
             echo Package manager: ${osInfo[$f]}
-            
+            # eval sudo ${osInfo[$f]} install node
         fi
     done
     node ./bin/server.js
